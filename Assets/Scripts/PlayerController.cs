84ba9457f65e24205ee8764ce1a273406ffdc22b
satocchi0416sh <83898438+satocchi0416sh@users.noreply.github.com>
2024-06-09 03:53:31 +0900

    feat: :rocket: プレイヤー移動

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 5f;
+    private void Update()
     {
-        
+        Move();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        transform.position += move * Time.deltaTime * speed;
     }
 }
